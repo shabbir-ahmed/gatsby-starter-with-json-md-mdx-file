5ff24287cd6e949ed90389b27ad71dd933eee630
shabbir-ahmed <9017121+shabbir-ahmed@users.noreply.github.com>
2023-04-09 13:50:41 +0600

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,28 +2,60 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `practice`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `practice`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		// "gatsby-plugin-google-gtag",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/icon.png",
+			},
+		},
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				extensions: [".mdx", ".md"],
+			},
+		},
+		"gatsby-transformer-remark",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+		`gatsby-transformer-json`,
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "data",
+				path: `${__dirname}/src/data/`,
+			},
+			__key: "data",
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `content`,
+				path: `${__dirname}/src/content`,
+			},
+		},
+	],
 };
